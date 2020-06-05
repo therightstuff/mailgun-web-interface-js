860bd64af1a7b24bb06a3410a016b65bbc57aede
therightstuff <adam@industrialcuriosity.com>
2020-06-06 00:16:36 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mailgun-web-interface-js
+
 A simple web interface for non-devs to send emails using mailgun
+
+The latest version can be loaded directly from the GitHub repo [here](http://htmlpreview.github.io/?https://github.com/therightstuff/mailgun-web-interface-js/blob/master/dist/index.html).
---
+++ src/mailgunLocal.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+/*
+	The node.js modules to be ported to the client must be added
+	to the window.mailgunLocal object.
+
+    "npm run build" must be run after updating this module
+
+    Once bundle has been included in html page with following directive
+        <script src="mailgunLocal.js"></script>
+    javascript calls can be made to mailgunLocal.* eg
+        let mailgun = window.mailgunLocal({
+            apiKey: credentials.apiKey,
+            domain: credentials.domain
+        });
+*/
+
+// update the browser's mailgunLocal object
+window.mailgunLocal = require('mailgun-js');
